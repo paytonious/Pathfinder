23d2cecf292695a8fff9c0f1562c5445b43dbd84
Payton Cox <paytonhcox@gmail.com>
2022-05-09 15:48:29 -0700

    Created skeleton as comment steps

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -1,1 +1,40 @@
-hello
+// Header files
+#include "cstdio"
+
+// Global constants
+
+// Main function
+int main() {
+   // Initialize variables
+
+   // Prompt for random or draw barriors
+
+      // if random set random flag to true
+
+      // else set random flag to false
+
+   // Display grid
+
+      // check if random flag is true
+      
+         // do random grid
+
+      // Otherwise assime random flag is false
+      
+         // do grid that can be interacted with
+
+   // Prompt for starting point
+
+   // Prompt for ending point
+
+   // Run dijkstra's algorithm
+
+   // Display end results
+
+      // Print how many tiles necessary for shortest path
+
+   // End Program
+   return 0;
+}
+
+// functions
